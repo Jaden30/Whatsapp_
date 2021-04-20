4c5b1e74ccfcb626a9ed9ecf6b45f105358700c9
Ola <austineolanrewaju@gmail.com>
2021-04-20 14:17:27 +0100

    Fixed the bug

--- app.py
+++ app.py
@@ -1,14 +1,38 @@
-from flask import Flask 
-
-
-
+from flask import Flask, request
+from twilio.twiml.messaging_response import MessagingResponse
+import requests 
 app = Flask(__name__)
 
+# turning it to lower because we 
 
-@app.route('\bot', methods=["POST"])
+url = ('https://newsapi.org/v2/top-headlines?'
+       'country=uk&'
+       'apiKey=575d1aa3b7474ee8bc0e86204ea5f89d')
+@app.route('/bot', methods=["POST"])
 def bot():
+    incoming_msg = request.values.get("Body", "").lower()
+    resp = MessagingResponse()
+    msg = resp.message()
+    responded = False
+  
+    if "news" in incoming_msg:
+        r = requests.get(url)
+        if r.status_code == 200:
+            data = r.json()
+            quote = f'{data["author"]} {data["title"]} {data["content"]}'
+        else: 
+            quote = "No news today"
+
+        msg.body(quote)
+        responded = True
+
+    if not responded : 
+        msg.body("Guess you do not want the news")
+    return str(resp)
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
+
+
 
